740e1f99bd2a79c89335c82d71897a9b2174aea1
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-23 04:15:19 +0900

    refactor: Button 컴포넌트 스타일링 불필요 코드 제거 및 로직 수정

--- src/components/shared/Button/styled.js
+++ src/components/shared/Button/styled.js
@@ -14,9 +14,10 @@ const getBtnColor = (colorType, isHover = false) => {
 
 export const StyledButton = styled.button`
     display: flex;
-    flex-direction: ${(props) => (props.$isIconRight ? "row" : "row-reverse")};
+    flex-direction: row;
     justify-content: center;
     align-items: center;
+    gap: 8px;
     height: 40px;
     border-radius: 4px;
     padding: 0 16px;
@@ -30,79 +31,9 @@ export const StyledButton = styled.button`
         background-color: ${(props) => getBtnColor(props.color, true)};
     }
 
-    // icons
-    & > i {
-        display: block;
+    & > svg {
         width: 18px;
         height: 18px;
-        ${(props) => (props.$isIconRight ? "margin-left: 6px" : "margin-right: 6px")};
-        background-size: cover;
     }
-
-    // 크기. default는 large
-    /* ${(props) =>
-        props.size === "medium" &&
-        css`
-            height: 2rem;
-            font-size: 0.75rem;
-
-            & > i {
-                width: 0.75rem;
-                height: 0.75rem;
-            }
-        `}
-    ${(props) =>
-        props.size === "small" &&
-        css`
-            height: 1.75rem;
-            font-size: 0.5rem;
-
-            & > i {
-                width: 0.6rem;
-                height: 0.6rem;
-            }
-        `} */
-
-    // $negative
-    /* ${(props) =>
-        props.$negative === true &&
-        css`
-            background-color: transparent;
-            border: 2px solid ${(props) => getBtnColor(props.color)};
-            color: ${(props) => getBtnColor(props.color)};
-
-            &:hover {
-                border-color: ${(props) => getBtnColor(props.color, true)};
-                background-color: ${(props) => getBtnColor(props.color, true)};
-                color: #fff;
-            }
-    `} */
-
-    /* &:disabled {
-        background-color: $disabled;
-        cursor: not-allowed;
-
-        &:hover {
-            background-color: $disabled;
-            color: #fff !important;
-            box-shadow: none;
-            border-width : 2px;
-        }
-
-        // $negative
-            ${(props) =>
-                props.$negative === true &&
-                css`
-                    background-color: transparent;
-                    border: 2px solid $disabled;
-                    
-                    &:hover {
-                        border-color: $disabled;
-                        //color: $disabled !important;
-                        background-color: transparent;
-                        border: 2px solid $disabled;
-                    }
-            `}
-    } */
 
 `;
