c9978944f41565da81f8908d2063c4978036e6ff
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-23 04:22:05 +0900

    design: Header 믹스인 적용 및 스타일링 수정

--- src/components/pages/Layout/Header/styled.js
+++ src/components/pages/Layout/Header/styled.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import {theme} from "../../../../styles/theme";
 
 export const StHeaderWrap = styled.header`
     width: 100%;    
     border-bottom: 1px solid #eaedef;
     position: ${props => props.$display ? "sticky" : "relative"};
     top: ${props => props.$display ? '-50px' : 0};
+    z-index: 1000;
 `;
 
 export const StHeaderBanner = styled.div`
@@ -13,6 +15,7 @@ export const StHeaderBanner = styled.div`
     background-color: #00bbff;
 `;
 export const StHeaderContainer = styled.div`
+    ${({ theme }) => theme.container('column')};
     width: 100%;
     height: 80px;
     padding: 10px 60px;
@@ -36,10 +39,11 @@ export const StMenuContainer = styled.ul`
     justify-content: flex-start;
     align-items: center;
     flex-direction: row;
+    gap: 30px;
+    margin-left: 40px;
 
     & > li {
         list-style: none;
-        margin: 0 20px;
         cursor: pointer;
         font-size: 18px;
         font-weight: 700;
@@ -54,5 +58,25 @@ export const StLogo = styled.div`
     display: inline-block;
     width: 74px;
     height: 34px;
-    background-color: #ccc;
+    & > img {
+        width: 100%;
+    }
+`;
+
+export const StHeaderSubMenuContainer = styled.ul`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+
+    & > li {
+        font-size: 14px;
+        padding: 0 10px;
+        border-right: 1px solid #EAEDEF;
+        cursor: pointer;
+
+        &:last-child {
+            border-right: none;
+        }
+    }
 `;
