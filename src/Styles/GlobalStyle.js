74b078dd76e0fcf5f836c938b52bfb58768824da
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-23 00:35:49 +0900

    design: GlobalStyle 전역 변수 추가

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+
+:root {
+	--color-main: #636e72;
+}
+
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -27,6 +32,7 @@ footer, header, hgroup, menu, nav, section {
 }
 body {
 	line-height: 1;
+	letter-spacing: -0.4px;
 }
 ol, ul {
 	list-style: none;
@@ -42,6 +48,13 @@ q:before, q:after {
 table {
 	border-collapse: collapse;
 	border-spacing: 0;
+}
+button {
+	border: 0;
+}
+a {
+	text-decoration: none;
+	color: inherit;
 }
 `;
 
