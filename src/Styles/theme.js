ab3559cb2f504a9e5d29b144cd9ced183aa3c514
Sujin Lee <soolovepat@naver.com>
2023-07-22 16:11:59 +0900

    feat: Add thumnail component

--- src/Styles/theme.js
+++ src/Styles/theme.js
@@ -1,42 +1,75 @@
 import { css } from "styled-components";
 
 const colors = {
-    whiteColor: "#ffffff",
-    lightGrayColor: "#dddddd",
-    mediumGrayColor: "#939393",
-    darkGrayColor: "#2F3438",
-    BlackColor: "#000000",
+  whiteColor: "#ffffff",
+  lightGrayColor: "#dddddd",
+  mediumGrayColor: "#939393",
+  darkGrayColor: "#2F3438",
+  BlackColor: "#000000",
 
-    primaryColor: "#35c5f0",
-    secondaryColor: "#FF7777",
+  primaryColor: "#35c5f0",
+  secondaryColor: "#FF7777",
 };
 
 const media = {
-    desktop: "(min-width: 1024px)",
-    tablet: "(min-width: 768px) and (max-width: 1023px)",
-    mobile: "(max-width: 767px)",
+  desktop: "(min-width: 1024px)",
+  tablet: "(min-width: 768px) and (max-width: 1023px)",
+  mobile: "(max-width: 767px)",
 };
 
-const mixins = {
-    container: () => `
-      max-width: 1256px;
-      margin: 0 auto;
-      padding: 0 60px;
-  `,
-};
+const house = css`
+  h1 {
+    margin: 18px 0 8px;
+    line-height: 24px;
+    font-size: 17px;
+    font-weight: 700;
+    word-break: keep-all;
+    word-wrap: break-word;
+  }
 
-const numberfont = css`
-  font-family: "Inter", sans-serif;
+  address {
+    margin: 8px 0;
+    font-size: 13px;
+    font-weight: 400;
+    line-height: 16px;
+  }
+
+  footer {
+    margin: 8px 0 0;
+    font-size: 13px;
+    line-height: 17px;
+    text-align: center;
+  }
 `;
 
-const theme = {
-    ...colors,
-    ...media,
-    ...mixins,
-  numberfont,
-  ...colors,
-  ...media,
+const product = css`
+  h1 {
+    margin-top: 9px;
+    line-height: 24px;
+    font-size: 13px;
+    font-weight: 300;
+    word-break: keep-all;
+    word-wrap: break-word;
+  }
+
+  > span {
+    margin-top: 2px;
+    font-size: 17px;
+    line-height: 23px;
+    font-weight: 700;
+  }
+`;
+
+const types = {
+  house,
+  product,
 };
 
+const theme = {
+  ...colors,
+  ...media,
+  ...types,
+  // h1box,
+};
 
 export default theme;
---
+++ src/components/pages/home/index.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import Thumbnail from "./thumbnail";
+import Banner from "./banner";
+import { StImgWrap, StThumbnailContainer } from "./styled";
+import { StContainer } from "../../../styles/Container";
+import { home_img_01, product_img_01 } from "../../../asset/images";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  const formData = {
+    articleList: [
+      {
+        articleId: 1,
+        title: "니집은 내꺼",
+        nickname: "항해의집",
+      },
+      {
+        articleId: 2,
+        title: "니집은 내꺼야",
+        nickname: "항해의집",
+      },
+      {
+        articleId: 3,
+        title: "니집은 내꺼다",
+        nickname: "항해의집",
+      },
+      {
+        articleId: 4,
+        title: "니집은 내꺼입니다",
+        nickname: "항해의집",
+      },
+    ],
+    itemList: [
+      {
+        itemId: 10,
+        itemname: "에이스 침대",
+        price: 30000,
+      },
+      {
+        itemId: 200,
+        itemname: "에이스 침대",
+        price: 30000,
+      },
+      {
+        itemId: 300,
+        itemname: "에이스 침대",
+        price: 30000,
+      },
+      {
+        itemId: 400,
+        itemname: "에이스 침대",
+        price: 30000,
+      },
+      {
+        itemId: 500,
+        itemname: "에이스 침대",
+        price: 30000,
+      },
+    ],
+  };
+
+  const onClickArticleHandler = (articleId) => {
+    navigate(`/articles/${articleId}`);
+  };
+
+  const onClickProductHandler = (productId) => {
+    navigate(`/products/${productId}`);
+  };
+
+  return (
+    <StContainer>
+      <Banner />
+      <StThumbnailContainer>
+        {formData.articleList.map((article) => (
+          <Thumbnail
+            key={article.articleId}
+            type={"house"}
+            column={"3"}
+            align={"center"}
+            onClick={() => onClickArticleHandler(article.articleId)}
+          >
+            <StImgWrap>
+              <img src={home_img_01} alt="home_img" />
+            </StImgWrap>
+            <h1>{article.title}</h1>
+            <address>{article.nickname}</address>
+            <footer>
+              <span>스크랩 11 · 조회 250</span>
+            </footer>
+          </Thumbnail>
+        ))}
+      </StThumbnailContainer>
+      <StThumbnailContainer>
+        {formData.itemList.map((item) => (
+          <Thumbnail
+            key={item.itemId}
+            type={"product"}
+            column={"4"}
+            align={"left"}
+            onClick={() => onClickProductHandler(item.productId)}
+          >
+            <StImgWrap>
+              <img src={product_img_01} alt="home_img" />
+            </StImgWrap>
+            <h1>
+              <span>거래처</span>
+              {item.itemname}
+            </h1>
+            <span>{item.price}</span>
+            {/* <footer>
+              <span>스크랩 11 · 조회 250</span>
+            </footer> */}
+          </Thumbnail>
+        ))}
+      </StThumbnailContainer>
+    </StContainer>
+  );
+};
+
+export default Home;
