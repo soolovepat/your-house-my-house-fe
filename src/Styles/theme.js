76cef93f2cb422e28eb87ae37e33d32e7036914a
Sujin Lee <soolovepat@naver.com>
2023-07-22 16:25:21 +0900

    style: move house, product css theme to styled

--- src/Styles/theme.js
+++ src/Styles/theme.js
@@ -1,4 +1,4 @@
-import { css } from "styled-components";
+// import { css } from "styled-components";
 
 const colors = {
   whiteColor: "#ffffff",
@@ -17,59 +17,9 @@ const media = {
   mobile: "(max-width: 767px)",
 };
 
-const house = css`
-  h1 {
-    margin: 18px 0 8px;
-    line-height: 24px;
-    font-size: 17px;
-    font-weight: 700;
-    word-break: keep-all;
-    word-wrap: break-word;
-  }
-
-  address {
-    margin: 8px 0;
-    font-size: 13px;
-    font-weight: 400;
-    line-height: 16px;
-  }
-
-  footer {
-    margin: 8px 0 0;
-    font-size: 13px;
-    line-height: 17px;
-    text-align: center;
-  }
-`;
-
-const product = css`
-  h1 {
-    margin-top: 9px;
-    line-height: 24px;
-    font-size: 13px;
-    font-weight: 300;
-    word-break: keep-all;
-    word-wrap: break-word;
-  }
-
-  > span {
-    margin-top: 2px;
-    font-size: 17px;
-    line-height: 23px;
-    font-weight: 700;
-  }
-`;
-
-const types = {
-  house,
-  product,
-};
-
 const theme = {
   ...colors,
   ...media,
-  ...types,
-  // h1box,
 };
 
 export default theme;
