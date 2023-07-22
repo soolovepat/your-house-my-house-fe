7677d9ad621ff012e6809e2d2b3f6c5d10e400fa
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-23 04:13:28 +0900

    feat: Layout 라우터 설정

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../components/pages/Layout"
 import Home from "../components/pages/home";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
