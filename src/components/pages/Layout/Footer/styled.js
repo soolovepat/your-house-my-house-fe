2fa1d6a6365c19dee92f31c0cccffd6667217e7a
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-23 12:16:01 +0900

    design: Footer 스타일링

--- src/components/pages/Layout/Footer/styled.js
+++ src/components/pages/Layout/Footer/styled.js
@@ -1,48 +1,129 @@
-import styled from 'styled-components';
-import {theme} from "../../../../styles/theme";
+import styled from "styled-components";
+import { theme } from "../../../../styles/theme";
 
 export const StFooterWrap = styled.footer`
-  width: 100%;
-  padding: 40px 0;
-  background-color: rgb(247, 249, 250);
-
-  & > div {
-    ${({ theme }) => theme.container('column')};
     width: 100%;
-    height: 100%;
+    height: 330px;
+    padding: 40px 0;
     box-sizing: border-box;
-    font-size: 12px;
-    line-height: 16px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: flex-start;
-    gap: 30px;
-  }
+    background-color: rgb(247, 249, 250);
 
-  nav {
-    color: rgb(47, 52, 56);
-    border-right: 1px solid rgb(234, 237, 239);
-    height: 100%;
-    padding-right: 40px;
+    & a:hover {
+      text-decoration: underline;
+    }
 
-    &:nth-child(2) {
+    & b {
+      font-weight: 700;
+    }
+
+    & > div {
+        ${({ theme }) => theme.container("column")};
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        font-size: 12px;
+        line-height: 16px;
         display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(6, min-content);
-        grid-auto-flow: column;
-        gap: 20px 10px;
+        grid-template-columns: 1fr 1fr 2fr;
+        column-gap: 30px;
     }
 
-    &:nth-child(3) {
-        font-size: 10px;
-        line-height: 14px;
-        display: flex;
-        flex-direction: column;
-        gap: 12px;
-        color: rgb(130, 140, 148);
-        border-right: 0;
-        padding-right: 0px;
+    nav {
+        height: 100%;
+        color: rgb(47, 52, 56);
+        border-right: 1px solid rgb(234, 237, 239);
+        padding-right: 30px;
+
+        &:nth-child(1) {
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          align-items: flex-start;
+          gap: 12px;
+
+          & > p {
+            font-size: 12px;
+            line-height: 20px;
+            white-space: pre-wrap;
+          }
+        }
+
+        &:nth-child(2) {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(6, min-content);
+            grid-auto-flow: column;
+            gap: 20px 10px;
+        }
+
+        &:nth-child(3) {
+            font-size: 12px;
+            line-height: 20px;
+            display: flex;
+            flex-direction: column;
+            gap: 12px;
+            color: #828C94;
+            border-right: 0;
+            padding-right: 0px;
+
+            & p, & address {
+                color: inherit;
+            }
+        }
     }
-  }
 `;
+
+export const StLinkCSCenter = styled.span`
+    font-size: 18px;
+    line-height: 24px;
+    font-weight: 700;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    margin-bottom: 10px;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    & > svg {
+      width: 12px;
+    }
+`;
+
+export const StLinkCSNumber = styled.span`
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 700;
+  margin-right: 6px;
+
+  &:hover {
+      text-decoration: underline;
+    }
+`;
+
+export const StLinkCSTime = styled.span`
+    font-size: 14px;
+    line-height: 20px;
+    font-weight: 400;
+`;
+
+export const StCSButton = styled.button`
+    width: fit-content;
+    height: 32px;
+    margin: 0px;
+    padding: 0px 8px;
+    background: none;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
+    box-sizing: border-box;
+    text-align: center;
+    color: rgb(47, 52, 56);
+    border: 1px solid rgb(218, 221, 224);
+    white-space: nowrap;
+    `;
