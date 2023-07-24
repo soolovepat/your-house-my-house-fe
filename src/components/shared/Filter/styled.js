92aabebfb9371135bf40ab0ef1cb58f68e043c64
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-24 15:52:22 +0900

    feat: Filter 컴포넌트 구현

---
+++ src/components/shared/Filter/styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const StFilter = styled.div`
+    width: fit-content;
+    font-size: 15px;
+    font-weight: 500;
+    line-height: 19px;
+    color: #757575;
+    padding: 7px 8px 6px;
+    border-radius: 4px;
+    background-color: #f5f5f5;
+    cursor: pointer;
+
+    & > svg {
+        width: 12px;
+        height: 12px;
+        margin-left: 5px;
+        vertical-align: -1px;
+    }
+`;
