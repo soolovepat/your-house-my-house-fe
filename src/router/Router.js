650b9f24a77ec6b3e955c7912ac9587e0e70c600
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-24 15:54:11 +0900

    집들이 리스트 컴포넌트 라우터 추가

--- src/router/Router.js
+++ src/router/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/pages/Layout"
 import Home from "../components/pages/home";
+import ArticleList from "../components/pages/ArticleList";
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/articles" element={<ArticleList />} />
         </Route>
       </Routes>
     </BrowserRouter>
