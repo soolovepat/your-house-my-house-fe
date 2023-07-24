bcc3e6f4c4cbb4c8fa5c5b7541619de998fbb1bb
Sujin Lee <soolovepat@naver.com>
2023-07-24 23:30:37 +0900

    refactor: move assets

--- public/assets/images/banner/index.js
+++
@@ -1,7 +0,0 @@
-export { default as banner01 } from "./banner01.avif";
-export { default as banner02 } from "./banner02.avif";
-export { default as banner03 } from "./banner03.avif";
-export { default as banner04 } from "./banner04.avif";
-export { default as banner05 } from "./banner05.avif";
-export { default as banner06 } from "./banner06.avif";
-export { default as bigbanner } from "./big-banner01.avif";
