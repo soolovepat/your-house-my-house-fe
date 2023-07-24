bf7a07208228d48fcb4b3db441c0d3083c4c9562
gunhoray <raylee9649@gmail.com>
2023-07-24 15:59:49 +0900

    feature/#3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Router from "./route/router";
 import "./App.css";
 import GlobalStyle from "./Styles/GlobalStyle";
+import LoginPage from "./pages/logninPage";
 
 function App() {
     return (
-        <>
+        <> 
+         <Router />
             <GlobalStyle />
             <div></div>
         </>
