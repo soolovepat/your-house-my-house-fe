31cf0e4a7660aef663230aa3682751dd6903caff
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-24 18:11:38 +0900

    feat: ArticleList 페이지 컴포넌트 구현 (진행중)

--- src/components/pages/ArticleList/styled.js
+++ src/components/pages/ArticleList/styled.js
@@ -1,36 +1,36 @@
 import styled from "styled-components";
 
-export const StArticleContainer = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 30px 20px;
-    margin-bottom: 40px;
+// export const StArticleContainer = styled.div`
+//     width: 100%;
+//     display: grid;
+//     grid-template-columns: 1fr 1fr 1fr;
+//     gap: 30px 20px;
+//     margin-bottom: 40px;
 
-    & > div {
-        width: 100%;
-        text-align: center;
+//     & > div {
+//         width: 100%;
+//         text-align: center;
 
-        & > img {
-            width: 100%;
-        }
-        & > h3 {
-            margin-top: 10px;
-            font-size: 20px;
-            line-height: 24px;
-            font-weight: 700;
-        } 
-        & > p {
-            margin-top: 10px;
-            font-size: 12px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: row;
-            gap: 5px;
-        }  
-    }
-`;
+//         & > img {
+//             width: 100%;
+//         }
+//         & > h3 {
+//             margin-top: 10px;
+//             font-size: 20px;
+//             line-height: 24px;
+//             font-weight: 700;
+//         } 
+//         & > p {
+//             margin-top: 10px;
+//             font-size: 12px;
+//             display: flex;
+//             justify-content: center;
+//             align-items: center;
+//             flex-direction: row;
+//             gap: 5px;
+//         }  
+//     }
+// `;
 
 export const StFilterContainer = styled.div`
     margin-top: 15px;
