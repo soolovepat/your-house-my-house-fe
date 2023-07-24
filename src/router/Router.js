ce56c7ad874932796c9dbffa56d7836d95e2f514
Sujin Lee <soolovepat@naver.com>
2023-07-24 18:52:52 +0900

    fix: style error for thumbnail container component

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "../components/pages/Layout"
+import Layout from "../components/pages/Layout";
 import Home from "../components/pages/home";
 
 const Router = () => {
