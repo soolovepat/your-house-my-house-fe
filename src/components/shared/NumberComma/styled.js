3e236b645dafc8e41b63e09558551cf9386c8a5c
Sujin Lee <soolovepat@naver.com>
2023-07-25 17:01:25 +0900

    remove: for rename error

--- src/components/shared/NumberComma/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StNumberComma = styled.span`
-  margin-top: 2px;
-  font-size: ${(props) => props.size};
-  line-height: ${(props) => props.lineheight};
-  font-weight: ${(props) => props.weight};
-  font-family: "Inter", sans-serif;
-`;
