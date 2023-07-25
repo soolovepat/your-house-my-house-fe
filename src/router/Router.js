4bf98d3836023f8d061915ba88b1cca487ab794c
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-26 00:17:25 +0900

    feat: router 추가

--- src/router/Router.js
+++ src/router/Router.js
@@ -4,6 +4,7 @@ import Layout from "../components/pages/layout/Layout";
 import Home from "../components/pages/home/Home";
 import Write from "../components/pages/write/Write";
 import ArticleList from "../components/pages/articleList/ArticleList";
+import ArticlePage from "../components/pages/articlePage/ArticlePage";
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/articles" element={<ArticleList />} />
+          <Route path="/articles/:id" element={<ArticlePage />} />
         </Route>
         <Route path="/write" element={<Write />} />
       </Routes>
