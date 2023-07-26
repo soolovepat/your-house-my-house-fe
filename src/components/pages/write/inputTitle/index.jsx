d4cfcf07c42093c47c41eb9defd71e5204c51747
mmms2000 <moseskmcmmms@gmail.com>
2023-07-27 04:23:45 +0900

    design: 제목인풋 빈값일때 빨간줄 완성

--- src/components/pages/write/inputTitle/index.jsx
+++ src/components/pages/write/inputTitle/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { styled } from "styled-components";
 import { useData } from "../hooks/useData";
+import { StTitleContainer } from "../container";
 
 function InputTitle() {
   const [title, setTitle] = useState('')
   const {data, setData} = useData()
+  const [touched, setTouched] = useState(false)
   
   const titleHandler = (e)=>{
     if(e.target.value.length <= 80) {
@@ -15,10 +17,19 @@ function InputTitle() {
 
   return (
     <>
-      <StTitleInput placeholder="제목을 입력해주세요" value={title} onChange={titleHandler} />
+    <StTitleContainer>
+      <StTitleInput
+       placeholder="제목을 입력해주세요" 
+       value={title} 
+       onChange={titleHandler} 
+       onBlur={() => setTouched(true)}
+       touched={touched}
+       />
       <StWordCount>{title.length}/80</StWordCount>
-
+    </StTitleContainer>
+    <div>{title === '' && touched && <StWarning>필수 입력 항목 입니다.</StWarning>} </div>
     </>
+    
   );
 }
 
@@ -35,6 +46,8 @@ const StTitleInput = styled.input`
   border: none;
   color: rgb(41, 41, 41);
   font-weight: bold;
+  border-bottom: ${props => (props.touched ? "1px solid  rgb(255, 119, 119)" : "none")};
+
   &:focus {
     outline: none;
   }
@@ -52,4 +65,11 @@ line-height: 19px;
 right: 0px;
 top: 50%;
 color: rgb(130, 140, 148); ;
+`
+const StWarning = styled.div`
+      margin: 10px 0px;
+    font-size: 12px;
+    line-height: 18px;
+    font-weight: bold;
+    color: rgb(255, 119, 119);
 `
