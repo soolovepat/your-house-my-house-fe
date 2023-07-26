46f9e8ff3d5c2fbed71c65e4643b1772054ddf45
Sujin Lee <soolovepat@naver.com>
2023-07-26 15:08:04 +0900

    style: 불필요한 코드 정리

--- src/components/pages/home/banner/Banner.jsx
+++ src/components/pages/home/banner/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { bigbanner } from "../../../../assets/images/banner";
 import Carousel from "../carousel/Carousel";
 import { StBanner, StSmallBanner } from "./styled";
