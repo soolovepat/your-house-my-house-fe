1b8bd7a9fbb185fb9c391f45c921ce5bd401193b
mmms2000 <moseskmcmmms@gmail.com>
2023-07-27 03:52:45 +0900

    design: 이미지 모달창 스타일 완료

--- src/components/pages/write/modals/imageModal/ImageCoverModal.jsx
+++ src/components/pages/write/modals/imageModal/ImageCoverModal.jsx
@@ -1,4 +1,5 @@
-import api from '../../../../../api/api'
+import { styled } from "styled-components";
+import api from "../../../../../api/api";
 import {
   StImageModalContainer,
   StImageModalWrapper,
@@ -8,29 +9,28 @@ import {
 import { StImageModalButton, StImagePreviewText } from "./styled";
 
 // Image open Modal----------
-const ImageCoverMdal = ({closeModal, setCoverImage}) => {
-
+const ImageCoverMdal = ({ closeModal, setCoverImage }) => {
   const coverImageHanler = (e) => {
     const input = document.createElement("input");
     input.setAttribute("type", "file");
     input.setAttribute("accept", "image/*");
     input.click();
-  
+
     input.onchange = async () => {
       const file = input.files[0];
       const formData = new FormData();
-      formData.append('coverImage', file);
+      formData.append("coverImage", file);
 
-      try{
-        const result = await api.post('/article/coverImage',formData,{
-          headers:{
+      try {
+        const result = await api.post("/article/coverImage", formData, {
+          headers: {
             "Content-Type": "multipart/form-data",
-          }
+          },
         });
         const cover_URL = result.data.url;
-        setCoverImage(cover_URL)
-      } catch (error){
-        console.log('Upload failed')
+        setCoverImage(cover_URL);
+      } catch (error) {
+        console.log("Upload failed");
       }
 
       closeModal();
@@ -40,7 +40,7 @@ const ImageCoverMdal = ({closeModal, setCoverImage}) => {
     <StOverlayed>
       <StImageModalContainer>
         <StImageModalWrapper>
-          <h1> 커버 사진 추가 하기</h1>
+          <Sttitle> 커버 사진 추가 하기</Sttitle>
           <div
             style={{
               flex: "0 0 auto",
@@ -56,10 +56,24 @@ const ImageCoverMdal = ({closeModal, setCoverImage}) => {
               현제 편집중인 글에서 사용된 이미지가 없습니다.
             </StImagePreviewText>
           </StImagePreview>
-          <div>
-            <button onClick={()=>closeModal()}>취소</button>
-            <button onClick={()=> window.alert('기능 구현중입니다')}>확인</button>
-          </div>
+          <StModalButtonWraper>
+            <StModalButton
+              backgroundColor="#ededed;"
+              borderColor="#ededed"
+              color="#757575"
+              onClick={() => closeModal()}
+            >
+              취소
+            </StModalButton>
+            <StModalButton
+              backgroundColor="#35c5f0"
+              borderColor="#35c5f0"
+              color="#fff"
+              onClick={() => window.alert("기능 구현중입니다")}
+            >
+              확인
+            </StModalButton>
+          </StModalButtonWraper>
         </StImageModalWrapper>
       </StImageModalContainer>
     </StOverlayed>
@@ -67,3 +81,36 @@ const ImageCoverMdal = ({closeModal, setCoverImage}) => {
 };
 
 export default ImageCoverMdal;
+
+const StModalButtonWraper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  padding: 6px 0px;
+`;
+
+// styled
+const StModalButton = styled.button`
+  background-color: ${(props) => props.backgroundColor};
+  border-color: ${(props) => props.borderColor};
+  color: ${(props) => props.color};
+  width: 100%;
+  max-width: 160px;
+  margin: 0 15px;
+  padding: 11px 10px 12px;
+  font-size: 17px;
+  border: 1px solid transparent;
+  text-align: center;
+  font-weight: 700;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+const Sttitle = styled.h1`
+  margin: 0px 0px 35px;
+  font-size: 20px;
+  line-height: 30px;
+  font-weight: bold;
+    color: rgb(41, 41, 41);
+`;
+
--- src/components/pages/write/modals/imageModal/styled.js
+++ src/components/pages/write/modals/imageModal/styled.js
@@ -1,28 +1,36 @@
 import { styled } from "styled-components";
 
-  // Image Modal Style
-  const StImageModalButton = styled.button.attrs({
-    className: "boder",
-  })`
-    max-width: 340px;
-    width: 100%;
-    padding: 14px 15px;
-    background-color: #828c94;
-    border-color: #828c94;
-    color: #fff;
-  
-    &.border {
-      border: 1px solid transparent;
-      display: inline-block;
-    }
-  `;
-  
-  const StImagePreviewText = styled.p`
-    padding: 60px 0px;
-    color: rgb(189, 189, 189);
-    font-size: 15px;
-    line-height: 23px;
-    text-align: center;
-  `;
-  
-  export {StImageModalButton,StImagePreviewText}
+// Image Modal Style
+const StImageModalButton = styled.button.attrs({
+  className: "boder",
+})`
+  max-width: 340px;
+  width: 100%;
+  padding: 14px 15px;
+  background-color: #828c94;
+  border-color: #828c94;
+  color: #fff;
+  font-size: 18px;
+  line-height: 30px;
+  font-weight: 700;
+  border-radius: 4px;
+  cursor: pointer;
+  &.border {
+    border: 1px solid transparent;
+    display: inline-block;
+  }
+  &:hover {
+    background-color: #656e75;
+    border-color: #656e75;
+  }
+`;
+
+const StImagePreviewText = styled.p`
+  padding: 60px 0px;
+  color: rgb(189, 189, 189);
+  font-size: 15px;
+  line-height: 23px;
+  text-align: center;
+`;
+
+export { StImageModalButton, StImagePreviewText };
