7ddf9cefd8e0e7365e341142b81ce78931a5545e
Sujin Lee <soolovepat@naver.com>
2023-07-26 19:30:21 +0900

    refactor: 폴더명 소문자로 변경

--- src/components/pages/ArticleList/filterContainer/FilterContainer.jsx
+++ src/components/pages/ArticleList/filterContainer/FilterContainer.jsx
@@ -4,65 +4,70 @@ import Dropdown from "../../../shared/dropdown/Dropdown";
 import { StFilterContainer, StFilteredInfoList } from "./styled";
 
 const FilterContainer = () => {
-
-    const filters = {
-        sorting: {
-            name: "정렬",
-            options: [
-                {
-                    label: "최근순",
-                    query: ["string", "newest"]
-                },
-                {
-                    label: "최근순",
-                    query: ["string", "newest"]
-                },
-                {
-                    label: "최근순",
-                    query: ["string", "newest"]
-                },
-                {
-                    label: "최근순",
-                    query: ["string", "newest"]
-                },
-            ]
+  const filters = {
+    sorting: {
+      name: "정렬",
+      options: [
+        {
+          label: "최근순",
+          query: ["string", "newest"],
         },
-        residence: {
-            name: "주거형태",
-            options: [
-                {
-                    label: "최근순",
-                    query: ["string", "newest"]
-                },
-                {
-                    label: "최근순",
-                    query: ["string", "newest"]
-                },
-                {
-                    label: "최근순",
-                    query: ["string", "newest"]
-                },
-                {
-                    label: "최근순",
-                    query: ["string", "newest"]
-                },
-            ]
+        {
+          label: "최근순",
+          query: ["string", "newest"],
         },
-    }
+        {
+          label: "최근순",
+          query: ["string", "newest"],
+        },
+        {
+          label: "최근순",
+          query: ["string", "newest"],
+        },
+      ],
+    },
+    residence: {
+      name: "주거형태",
+      options: [
+        {
+          label: "최근순",
+          query: ["string", "newest"],
+        },
+        {
+          label: "최근순",
+          query: ["string", "newest"],
+        },
+        {
+          label: "최근순",
+          query: ["string", "newest"],
+        },
+        {
+          label: "최근순",
+          query: ["string", "newest"],
+        },
+      ],
+    },
+  };
 
-    return (
-        <>
-        <StFilterContainer>
-            <Filter filterInfo={filters.sorting} />
-            <Filter filterInfo={filters.residence} />
-        </StFilterContainer>
-        <StFilteredInfoList>
-            <li><button>빌라 연립</button></li>
-            <li><button>역대 인기순</button></li>
-            <li><button>초기화</button></li>
-        </StFilteredInfoList>
-        </>
-    );
+  return (
+    <>
+      <StFilterContainer>
+        <Filter filterInfo={filters.sorting} />
+        <Filter filterInfo={filters.residence} />
+      </StFilterContainer>
+      <StFilteredInfoList>
+        <li>
+          <button>빌라 연립</button>
+        </li>
+        <li>
+          <button>역대 인기순</button>
+        </li>
+        <li>
+          <button>초기화</button>
+        </li>
+      </StFilteredInfoList>
+    </>
+  );
 };
 
 export default FilterContainer;
--- src/components/shared/Dropdown/Dropdown.jsx
+++ src/components/shared/Dropdown/Dropdown.jsx
@@ -1,37 +1,41 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { StDropdown } from "./styled";
 
 const Dropdown = (props) => {
+  const location = useLocation();
 
-    const location = useLocation();
+  const handleQuery = (query, value) => {
+    console.log(query, value);
+  };
 
-    const handleQuery = (query, value) => {
-        console.log(query, value);
-    }
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
 
-    useEffect(() => {
-        console.log(location);
-    }, [location]);
+  const { isOpened, filterInfo } = props;
 
-    const { isOpened, filterInfo } = props;
+  console.log(filterInfo);
 
-    console.log(filterInfo)
-
-    return (
+  return (
     <StDropdown $isOpened={isOpened}>
-        <ul>
-            {filterInfo.options.map((item, index) => {
-                return (
-                    <li key={index} onClick={() => {handleQuery(item.query[0], item.query[1])}}>
-                        {filterInfo.name}
-                    </li>
-                );
-            })}
-        </ul>
+      <ul>
+        {filterInfo.options.map((item, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => {
+                handleQuery(item.query[0], item.query[1]);
+              }}
+            >
+              {filterInfo.name}
+            </li>
+          );
+        })}
+      </ul>
     </StDropdown>
-    );
+  );
 };
 
 export default Dropdown;
