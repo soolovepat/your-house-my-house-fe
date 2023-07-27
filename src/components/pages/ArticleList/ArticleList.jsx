8eef46f111ab7953250c4f2b5ba444e3d9ff95ad
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-27 14:53:15 +0900

    fix: ArticleList 목업 데이터 -> 서버로 들어오게

--- src/components/pages/ArticleList/ArticleList.jsx
+++ src/components/pages/ArticleList/ArticleList.jsx
@@ -17,34 +17,11 @@ const ArticleList = () => {
       .catch(err => console.log(err));
     },[articles]);
 
-  const articleList = [
-    {
-      articleId: 1,
-      title: "니집은 내꺼",
-      nickname: "항해의집",
-    },
-    {
-      articleId: 2,
-      title: "니집은 내꺼야",
-      nickname: "항해의집",
-    },
-    {
-      articleId: 3,
-      title: "니집은 내꺼다",
-      nickname: "항해의집",
-    },
-    {
-      articleId: 4,
-      title: "니집은 내꺼입니다",
-      nickname: "항해의집",
-    },
-  ];
-
   return (
     <StArticleWrapper>
       <FilterContainer />
-      <p style={{ margin: "30px 0 10px" }}>전체: {articleList.length}</p>
-      <HouseContainer articleList={articleList} column="3" />
+      <p style={{ margin: "30px 0 10px" }}>전체: {articles.length}</p>
+      <HouseContainer articleList={articles} column="3" />
     </StArticleWrapper>
   );
 };
